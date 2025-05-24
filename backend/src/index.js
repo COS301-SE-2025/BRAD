87eafa20089173fd059ccf6e6cfc8c3a93874a36
Carinda Smith <u22652974@tuks.co.za>
2025-05-24 13:57:29 +0200

    added a test for database and backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,3 +17,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const testRoutes = require('./routes/test');
+app.use('/api', testRoutes);
