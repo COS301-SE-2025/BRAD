741f1bc959d7988b5dfed7086193968ec5653299
u19364742 <u19364742@DESKTOP-C451FLK.localhost>
2025-05-25 12:38:50 +0200

    Push all recent modifications to api branch

--- swagger.js
+++ swagger.js
@@ -1,19 +1,9 @@
-const swaggerJsdoc = require("swagger-jsdoc");
 const yaml = require("js-yaml");
 const fs = require("fs");
 const path = require("path");
 
-const swaggerDoc = yaml.load(fs.readFileSync(path.join(__dirname, "docs/openapi.yaml"), "utf8"));
-
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "B.R.A.D API",
-      version: "1.0.0",
-    },
-  },
-  apis: ["./routes/*.js", "./docs/*.yaml"],
-};
+const swaggerDoc = yaml.load(
+  fs.readFileSync(path.join(__dirname, "docs/openapi.yaml"), "utf8")
+);
 
 module.exports = swaggerDoc;
