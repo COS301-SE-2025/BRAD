98fb1bb1b6d73a047740b31d0ee0c1b22b04f65f
Tebatso Sophy Mahlathini <u22611704@tuks.co.za>
2025-08-08 23:29:58 +0200

    added few endpoints

--- api/src/report/report.controller.ts
+++ api/src/report/report.controller.ts
@@ -30,7 +30,7 @@ export class ReportController {
   @Post('report')
   @UseInterceptors(FilesInterceptor('evidence',5,{
     storage: diskStorage({
-     destination: path.join(__dirname, '..', '..','..', '..', '..','..','uploads', 'evidence'),
+     destination: path.join(__dirname, '..', '..','uploads', 'evidence'),
 
       
       // Ensure the uploads directory exists
